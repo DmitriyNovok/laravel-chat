22c0923c50f82792fcfbf32bcec281021f6e1b80
DmitriyNovok <d.novokreshennyh@key-g.com>
2024-04-14 14:19:18 +0300

    Create console command GitPull

---
+++ app/Console/Commands/GitPull.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GitPull extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'git:pull {branch}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get changes from branch';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $newBranch = $this->argument('branch');
+
+        $this->info(shell_exec('php artisan down'));
+        $this->info(shell_exec('git clean -f -d'));
+        $this->info(shell_exec('git reset --hard HEAD'));
+        $this->info(shell_exec("git checkout $newBranch"));
+        $this->info(shell_exec("git pull origin $newBranch"));
+        $this->info(shell_exec('php artisan migrate --force'));
+        $this->info(shell_exec('php artisan config:clear'));
+        $this->info(shell_exec('php artisan up'));
+    }
+}
