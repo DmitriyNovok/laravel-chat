9b36e25194be84893d5d6a8385fa43606f4ddbd5
DmitriyNovok <d.novokreshennyh@key-g.com>
2024-05-11 09:51:27 +0300

    Fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ auth()->check() ? url('/channels/'.request()->segments()[1]) : url('/') }}"><?= auth()->check() ? \App\Models\Channel::where('slug', request()->segments()[1])->first()?->name : '' ?></a>
+                <a class="navbar-brand" href="{{ (auth()->check() && count(request()->segments()) > 0) ? url('/channels/'.request()->segments()[1]) : url('/') }}"><?= (auth()->check() && count(request()->segments()) > 0) ? \App\Models\Channel::where('slug', request()->segments()[1])->first()?->name : '' ?></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
