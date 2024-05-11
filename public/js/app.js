b153ea5309ae4a59f8f5dd5cceffd8bce1c09b56
DmitriyNovok <d.novokreshennyh@key-g.com>
2024-05-11 18:44:06 +0300

    Settings echo server

--- public/js/app.js
+++ public/js/app.js
@@ -5456,7 +5456,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.io = __webpack_require__(/*! socket.io-client */ "./node_modules/socket.io-client/lib/index.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'socket.io',
-  host: window.location.hostname + ':8443',
+  host: window.location.hostname + ':6001',
   transports: ['websocket', 'polling', 'flashsocket']
 });
 
